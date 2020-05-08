cbffeb5bf8f758f2ecf9be19876749fced41d450
streicherlouw <streicher@streicher.dk>
2020-05-08 20:41:01 +1000

    Update step3_start_background_build.sh

--- step3_start_background_build.sh
+++ step3_start_background_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Please enter the sudo password"
 read -sp 'Password: ' PW
-nohup sh ~/fastai2_jetson_nano/step3_install_fastai2.sh $PW & 
+cd ~/
+nohup ./fastai2_jetson_nano/step3_install_fastai2.sh $PW & 
 tail -f nohup.out
