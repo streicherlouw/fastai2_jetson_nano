5f17687daba90d98a37b23558b8a490f0ad4de96
streicherlouw <streicher@streicher.dk>
2020-05-08 20:35:32 +1000

    Update step3_install_fastai2.sh

--- step3_install_fastai2.sh
+++ step3_install_fastai2.sh
@@ -120,7 +120,6 @@ export USE_PYTORCH_QNNPACK=0
 export TORCH_CUDA_ARCH_LIST="5.3"
 export PYTORCH_BUILD_VERSION=1.5.0
 export PYTORCH_BUILD_NUMBER=1
-export BLAS=OpenBLAS # We have to tell Pytorch not to link to the included MKL
 python3 setup.py bdist_wheel
 pip3 install dist/torch-1.5.0-cp36-cp36m-linux_aarch64.whl
 cd ~/
