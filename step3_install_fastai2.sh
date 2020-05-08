2fce57f100b0b6a9619bb2ea04c1f9767a6be623
streicherlouw <streicher@streicher.dk>
2020-05-08 14:56:54 +1000

    Update step3_install_fastai2.sh

--- step3_install_fastai2.sh
+++ step3_install_fastai2.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 # Install Fastai2 on Nvidia Jetson Nano running Jetpack 4.4
 # Authored by Streicher Louw, April 2020, based on previous work by
-# Bharat Kunwar https://github.com/brtknr
-# and Jeffrey Antony https://github.com/jeffreyantony
+# Bharat Kunwar https://github.com/brtknr (installation of fastai) 
+# and Jeffrey Antony https://github.com/jeffreyantony (use of TMUX)
 
 # With a fast SD card, this process takes around 12-16 hours.
-
-# Step 1: Flash Jetpack 4.4
-# Flash an SD card with Jetpack 4.4 as described on
-# https://developer.nvidia.com/embedded/jetpack
-# and complete the intitial setup of username and password using a screen and
-# keyboard connected directly to the jetson. My jetson did not work reliably
-# with a 4K screen. Use an HD screen or TV if you can.
-# Open a terminal and download the files you will need for this process from github
-# with the command "git clone https://github.com/streicherlouw/fastai2_jetson_nano"
-
-# Step 2: Setup swap space and disable the GUI
-# Compiling pytorch uses more memory than the jetson nano has. To bridge the gap
-# we can add some swap space that will use the SD card as additional memory.
-# Swapping to disk is very slow though, so to free to as much memory as we can
-# we also need to switch off the Graphical User Interface by tellign the nano to
-# stop booting when it reaches runlevel 3, where it presents a text terminal.
-# Both of these functions are performed by the script step2_setup_swap_and_textmode.sh. To
-# run this script, we first needs to mark it as executable, and then run it and
-# reboot as follows:
-# "chmod +x ~/fastai2_jetson_nano/tep2_setup_swap_and_textmode.sh"
-# "sudo ./fastai2_jetson_nano/tep2_setup_swap_and_textmode.sh"
-# The nano should then reboot and present you with a text terminal to log into.
-
-# Step 3: Install dependencies and compile Pytorch and FastAI
-# For the next part, you will either need to log into the nano by ssh or the
-# the local console terminal. If using ssh, make sure the connection remains open
-# for the entire time the script is running. If your terminal quits (for example
-# because the computer you are connecting from goes to sleep) the terminal session
-# will end and the installation will stop midway.
-# To start the installation, type "./fastai2_jetson_nano/step3_install_fastai2.sh"
 
 # As this script will take many hours to execute, the script first needs to
 # cache your sudo credentials if they are not supplied on the command line
