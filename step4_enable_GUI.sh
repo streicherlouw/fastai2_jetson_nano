ac16b3f64aad4becdddbd8bf60fc87f0fbab1569
streicherlouw <streicher@streicher.dk>
2020-05-19 14:09:16 +1000

    Update step4_enable_GUI.sh

--- step4_enable_GUI.sh
+++ step4_enable_GUI.sh
@@ -1,4 +1,8 @@
+
 #!/bin/bash
+# Install FastAI2 on Jetpack 4.4. 
+# Step 4: Optional script to re-enable GUI
+
 sudo systemctl enable graphical.target
 sudo systemctl set-default graphical.target
 echo "The graphical user interface has been re-enabled, the system will restart now."
