7362b1dd51dab6869c6eeeb1a570330453d417f2
streicherlouw <streicher@streicher.dk>
2020-05-19 16:01:45 +1000

    Update step3_install_fastai2_background.sh

--- step3_install_fastai2_background.sh
+++ step3_install_fastai2_background.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo "Please enter the sudo password"
+echo "These prompts will only ask once, please take care to type the passwords correctly"
+echo "Please enter the sudo password:"
 read -sp 'Password: ' PW
+echo "Please enter your desired jupyter notebook password:"
+read -sp 'Password: ' JPW
+
 cd ~/
-nohup ./fastai2_jetson_nano/step3_install_fastai2.sh $PW &
-sleep 1
+nohup ./fastai2_jetson_nano/step3_install_fastai2.sh $PW $JPW &
+sleep 2
 tail -f ~/nohup.out
