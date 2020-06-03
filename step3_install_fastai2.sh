7686f322576b56d31b3783407db8f98682365927
streicherlouw <streicher@streicher.dk>
2020-06-03 11:14:39 +1000

    Update step3_install_fastai2.sh

--- step3_install_fastai2.sh
+++ step3_install_fastai2.sh
@@ -30,6 +30,7 @@ now=`date`
 echo "Start Installation of fastai2 on jetson nano at: $now"
 
 # Update the nano's software
+echo $PW | sudo -k --stdin apt -y install apt-utils
 echo $PW | sudo -k --stdin apt -y update
 echo $PW | sudo -k --stdin apt -y upgrade
 echo $PW | sudo -k --stdin apt -y autoremove
@@ -39,12 +40,13 @@ echo $PW | sudo -k --stdin apt -y autoremove
 
 echo $PW | sudo -k --stdin apt install -y python3-pip
 
-pip3 install wheel
-# Sudo upgrading pip a super ugly workaround, as it breaks pip in some fundemental ways
-# see (https://github.com/pypa/pip/issues/5599), but is the recommended procedure for 
-# building pytorch given by nvidia (https://forums.developer.nvidia.com/t/pytorch-for-jetson-nano-version-1-5-0-now-available/72048)
+# Using sudo to upgrade pip with pip a super ugly workaround, as it breaks pip in 
+# some fundemental ways see (https://github.com/pypa/pip/issues/5599), but is
+# the recommended procedure for building pytorch given by nvidia 
+# (https://forums.developer.nvidia.com/t/pytorch-for-jetson-nano-version-1-5-0-now-available/72048)
 # This is fixed again at the end of the setup stript by reinstalling the distribution pip
-echo $PW | sudo -k --stdin pip3 install -U setuptools 
+echo $PW | sudo -k --stdin -H pip3 install -U setuptools 
+echo $PW | sudo -k --stdin -H pip3 install -U pip
 
 # Install MAGMA from source
 # Since fastai requires pytorch to be compiled MAGMA, MAGMA needs to be installed first
@@ -73,7 +75,7 @@ echo $PW | sudo -k --stdin --preserve-env make install prefix=/usr/local/magma
 now=`date`
 echo "Start first run of MAGMA at: $now"
 cd ~/magma-2.5.3/testing
-python2 run_tests.py --precision s --small --no-mgpu
+python2 run_tests.py --lu --precision s --small --no-mgpu
 cd ~/
 
 now=`date`
@@ -127,6 +129,7 @@ pip3 install matplotlib
 # Install dependencies for py torch build
 pip3 install scikit-build --user
 pip3 install ninja --user
+pip3 install wheel
 
 # Build torch from source
 now=`date`
@@ -165,19 +168,14 @@ git clone https://github.com/fastai/fastcore # install fastcore
 pip3 install nbdev
 pip3 install dataclasses
 cd ~/fastcore
-# The fastai installation script, for some reason, does not recognise pytorch when installed 
-# without a virtual evironment, so we need to tell it to ignore dependencies, which are installed above
 pip3 install -e ".[dev]"
 cd ~/                                      
-
 
 pip3 install fastprogress
 
 git clone https://github.com/fastai/fastai2 # install fastai and patch augment.py
 cd ~/fastai2
 patch -p1 < ~/fastai2_jetson_nano/fastai2_torch_1_5_0.patch
-# The fastai installation script, for some reason, does not recognise pytorch when installed 
-# without a virtual evironment, so we need to tell it to ignore dependencies, which are installed above
 pip3 install -e ".[dev]"
 cd ~/
 
@@ -216,7 +214,7 @@ JPWHash=$(python3 -c "from notebook.auth import passwd; print(passwd('$JPW'))")
 echo "{\"NotebookApp\":{\"password\":\"$JPWHash\"}}" >> ~/.jupyter/jupyter_notebook_config.json
 
 # Reinstalling the distribution pip to undo sudo pip upgrade kludge (https://github.com/pypa/pip/issues/5599)
-echo $PW | sudo -k --stdin python3 -m pip uninstall pip 
+echo $PW | sudo -k --stdin python3 -m pip uninstall -y pip 
 echo $PW | sudo -k --stdin apt install python3-pip --reinstall
 
 echo "Installation Completed"
